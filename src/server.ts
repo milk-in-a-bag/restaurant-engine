4cb11e5340a9933962395e5b3cc1ffe40441c525
milk-in-a-bag <bryanmayodi@gmail.com>
2026-07-24 03:58:55 +0300

    set up express and typescript

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = 4000;
+
+app.get("/health", (req, res) => {
+  res.json({ status: "ok" });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
